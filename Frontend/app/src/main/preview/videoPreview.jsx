cded919b5ee51ab5f5c38e150ae12dccf1f15e16
Kevin <kern3985@gmail.com>
2026-02-24 08:00:37 -0800

    video player

--- Frontend/app/src/main/preview/videoPreview.jsx
+++ Frontend/app/src/main/preview/videoPreview.jsx
@@ -1,8 +1,11 @@
 import { Typography, Paper, Divider, Button, Box } from '@mui/material';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { fetchFile } from '../../services/services';
+import ReactPlayer from 'react-player';
 
 export function VideoPreview({ path, selectedVideo }) {
+  const playerRef = useRef(null);
+
   useEffect(() => {
     async function getData() {
       let video = await fetchFile(path + selectedVideo);
@@ -11,9 +14,21 @@ export function VideoPreview({ path, selectedVideo }) {
   }, [selectedVideo]);
 
   return (
-    <Box>
-      <p>path:{path}</p>
-      <p>selectedVideo:{selectedVideo}</p>
+    <Box height={'100%'}>
+      <ReactPlayer
+        width={'100%'}
+        height={'100%'}
+        ref={playerRef}
+        controls
+        pip={false}
+        onTimeUpdate={(e, i) => {
+          //console.log(e.target.currentTime);
+          console.log(playerRef.current.currentTime);
+          //playerRef.current.currentTime = 10;
+          //playerRef.current = 100;
+        }}
+        src="http://localhost:8080/video?path=./../../../Screen%20Recording%202026-02-03%20at%2011.02.46%E2%80%AFAM.mov"
+      />
     </Box>
   );
 }
