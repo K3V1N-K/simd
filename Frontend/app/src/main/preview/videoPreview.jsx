3ce18615e3e52ce873ebc2e666d1de70c389091e
Kevin <kern3985@gmail.com>
2026-02-25 09:59:01 -0800

    Update videoPreview.jsx

--- Frontend/app/src/main/preview/videoPreview.jsx
+++ Frontend/app/src/main/preview/videoPreview.jsx
@@ -1,27 +1,115 @@
-import { Typography, Paper, Divider, Button, Box } from '@mui/material';
-import { useEffect, useRef } from 'react';
+import {
+  Typography,
+  Paper,
+  Divider,
+  Button,
+  Box,
+  CircularProgress,
+  Modal,
+} from '@mui/material';
+import { useEffect, useRef, useState } from 'react';
 import { fetchFile } from '../../services/services';
 import ReactPlayer from 'react-player';
+import CanvasDraw from 'react-canvas-draw';
 
-export function VideoPreview({ path, selectedVideo, playerRef }) {
-  useEffect(() => {}, [selectedVideo]);
+import { VideoCtrl } from './videoCtrl';
+import { DrawingLayer } from './drawingLayers';
+
+export function VideoPreview({
+  path,
+  selectedVideo,
+  playerRef,
+  layers,
+  activeLayerId,
+}) {
+  const [videoReady, setVideoReady] = useState(false);
+  const [isFirstLoad, setIsFirstLoad] = useState(true);
+
+  const [secondsPlayed, setSecondsPlayed] = useState(0);
+  const [isVideoPlaying, setIsVideoPlaying] = useState(0);
+  const [playVideo, setPlayVideo] = useState(true);
+
+  useEffect(() => {
+    setIsFirstLoad(true);
+  }, [path]);
 
   return (
     <Box height={'100%'}>
-      <ReactPlayer
-        width={'100%'}
-        height={'100%'}
-        ref={playerRef}
-        controls
-        pip={false}
-        onTimeUpdate={(e, i) => {
-          //console.log(e.target.currentTime);
-          console.log(playerRef.current);
-          //playerRef.current.currentTime = 10;
-          //playerRef.current = 100;
-        }}
-        src="http://localhost:8080/video?path=./../../../Screen%20Recording%202026-02-03%20at%2011.02.46%E2%80%AFAM.mov"
+      <Box height={'100%'}>
+        <ReactPlayer
+          width={'100%'}
+          height={'calc(100% - 3em)'}
+          ref={playerRef}
+          pip={false}
+          playing={playVideo}
+          onPlay={() => {
+            setIsVideoPlaying(true);
+            console.log('play');
+            setIsFirstLoad;
+          }}
+          onPause={() => {
+            setIsVideoPlaying(false);
+          }}
+          onTimeUpdate={(e) => {
+            if (isFirstLoad) {
+              playerRef.current.playing = false;
+              setIsFirstLoad(false);
+              setVideoReady(true);
+              setPlayVideo(false);
+              console.log(playerRef);
+            }
+            setSecondsPlayed(playerRef.current.currentTime);
+          }}
+          src={
+            selectedVideo
+              ? `http://localhost:8080/video?path=${path.replaceAll(' ', '%') + selectedVideo}`
+              : null
+          }
+        />
+        <VideoCtrl
+          video={playerRef.current}
+          secondsPlayed={secondsPlayed}
+          isVideoPlaying={isVideoPlaying}
+          playVideo={playVideo}
+          setPlayVideo={setPlayVideo}
+        />
+      </Box>
+
+      <DrawingLayer
+        videoPlayer={playerRef.current}
+        videoReady={videoReady}
+        layers={layers}
+        activeLayerId={activeLayerId}
       />
+
+      <Modal
+        open={!videoReady && selectedVideo}
+        sx={{
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          height: '9em',
+        }}
+      >
+        <Paper
+          sx={{
+            flex: 1,
+            height: '100%',
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            flexDirection: 'column',
+            p: 2,
+          }}
+        >
+          <Typography>Video Loading</Typography>
+          <CircularProgress />
+          <Typography>{selectedVideo}</Typography>
+        </Paper>
+      </Modal>
     </Box>
   );
 }
+
+//<CanvasDraw hideGrid={true} canvasWidth={'calc(100vw - 20em)'} />
